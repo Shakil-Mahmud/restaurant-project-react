3de185d601118e2d93139e65b504e5eb6cb5f16e
Shakil-Mahmud <shakilmahmud13@gmail.com>
2020-11-29 23:41:39 +0600

    Component tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Mainconponent from './components/MainComponent'
 
 function App() {
   return (
     <div className="App">
-
+      <Mainconponent />
     </div>
   );
 }
